6cd637c9ccc52221d98d56df49f06de5431c76d4
Edwin-Jimenez12 <ed.jimenez0012@gmail.com>
2026-08-05 16:49:59 -0500

    fix: aun arreglando imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ function App() {
     <div className="flex h-dvh flex-col overflow-hidden bg-[#000000]">
       <Menu className="absolute" />
       <div>
-        <div className="relative">
+        <div className="relative ">
           <Inicio/>
           {/* Laptop */}
           <img src="/laptop.svg" alt="Laptop"
             className="absolute lg:right-[6%] lg:bottom-[14%] md:right-[5%] md:bottom-[25%] 
-            lg:w-175 md:w-100 w-50  md:flex hidden"
+            lg:w-175 md:w-100 w-50  md:flex hidden transition-all duration-300"
           />
           {/* Celular */}
           <img src="/celular.svg" alt="Celular"
             className="absolute lg:right-[5%] lg:bottom-[14%] md:right-[5%] md:bottom-[25%] 
-            rotate-3 lg:w-35 md:w-20 w-10 z-10 md:flex hidden"
+            rotate-3 lg:w-35 md:w-20 w-10 z-10 md:flex hidden transition-all duration-300"
           />
         </div>
       </div>
